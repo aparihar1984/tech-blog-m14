91d13da8695406c40e1b517a6349c462fd47fe5d
ashok parihar <aparihar@email.arizona.edu>
2024-04-26 21:44:17 -0600

    Adding the get methods to the post and user routes files.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,6 +2,71 @@ const router = require('express').Router();
 const { User, Post, Comment  } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// Using the GET method to get all the posts
+router.get("/", (req, res) => {
+  Post.findAll({
+          attributes: ["id", "content", "title", "created_at"],
+          order: [
+              ["created_at", "DESC"]
+          ],
+          include: [{
+                  model: User,
+                  attributes: ["username"],
+              },
+              {
+                  model: Comment,
+                  attributes: ["id", "comment", "post_id", "user_id", "created_at"],
+                  include: {
+                      model: User,
+                      attributes: ["username"],
+                  },
+              },
+          ],
+      })
+      .then((dbPostData) => res.json(dbPostData))
+      .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+// Using the GET method to get a single post
+router.get("/:id", (req, res) => {
+  Post.findOne({
+          where: {
+              id: req.params.id,
+          },
+          attributes: ["id", "content", "title", "created_at"],
+          include: [{
+                  model: User,
+                  attributes: ["username"],
+              },
+              {
+                  model: Comment,
+                  attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                  include: {
+                      model: User,
+                      attributes: ["username"],
+                  },
+              },
+          ],
+      })
+      .then((dbPostData) => {
+          if (!dbPostData) {
+              res.status(404).json({
+                  message: "No post found with this id"
+              });
+              return;
+          }
+          res.json(dbPostData);
+      })
+      .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+
 // Using the POST method to create a new post ('/api/post')
 router.post('/', withAuth, async (req, res) => {
     try {
